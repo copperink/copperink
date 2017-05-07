3f228f3f157e20f651ef3392d84d8a98e500adb9
Sheharyar Naseer <hello@sheharyar.me>
2017-05-07 03:26:46 -0400

    Fetching postable accounts in Facebook service

--- app/services/social_service/facebook.rb
+++ app/services/social_service/facebook.rb
@@ -1,5 +1,38 @@
 class SocialService::Facebook < SocialService::Base
-  def self.to_sym
-    :facebook
+  POST_PERMISSION = 'CREATE_CONTENT'.freeze
+  PICTURE_FIELD   = 'picture.type(large){url}'.freeze
+  ACCOUNT_FIELDS  = %W(id name #{PICTURE_FIELD} accounts{access_token,name,perms,#{PICTURE_FIELD}}).freeze
+  ACCOUNT_TYPES   = %w(profile, :page).freeze
+
+
+  def self.to_sym;   :facebook;   end
+
+
+  def self.accounts(token)
+    data =
+      Koala::Facebook::API
+        .new(token)
+        .get_object('me', fields: ACCOUNT_FIELDS)
+
+    accounts =
+      data['accounts']['data']
+        .select { |a| a['perms'].include?(POST_PERMISSION) }
+        .map    { |a| normalize_account_data(a, 'page')    }
+
+    accounts.unshift(normalize_account_data(data))
+  end
+
+
+
+  private
+
+  def self.normalize_account_data(data, type = 'profile')
+    {
+      id:     data['id'],
+      name:   data['name'],
+      image:  data['picture']['data']['url'],
+      token:  data['access_token'],
+      type:   type
+    }
   end
 end
